a5354959498feeedb48d8457e53fb8d7d7376a65
Anthony Tyrony Pereira Da Silva <anthony.pereira@resourceit.com>
2020-09-09 21:39:23 -0300

    Aula 05 - Exercício 2 - Nível 2 - Decisão

---
+++ src/Aula05Ex2.kt
@@ -0,0 +1,43 @@
+
+fun main() {
+    print("Quanto você quer sacar: ")
+    var valorSaque = readLine()!!.toInt()
+
+    var quantidadeNotaCem = 0
+    var quatidadeNotaCinquenta = 0
+    var quantidadeNotaDez = 0
+    var quantidadeNotaCinco = 0
+    var quantidadeNotaUm = 0
+    if (valorSaque < 10 || valorSaque > 600) {
+        print("Valor de saque não permitido")
+    } else {
+        if (valorSaque >= 100) {
+            quantidadeNotaCem = valorSaque / 100
+            valorSaque -= (quantidadeNotaCem * 100)
+            println("$quantidadeNotaCem notas de 100")
+        }
+
+        if (valorSaque >= 50) {
+            quatidadeNotaCinquenta = valorSaque / 50
+            valorSaque -= (quatidadeNotaCinquenta * 50)
+            println("$quatidadeNotaCinquenta notas de 50")
+        }
+
+        if (valorSaque >= 10) {
+            quantidadeNotaDez = valorSaque / 10
+            valorSaque -= (quantidadeNotaDez * 10)
+            println("$quantidadeNotaDez notas de 10")
+        }
+
+        if (valorSaque >= 5) {
+            quantidadeNotaCinco = valorSaque / 5
+            valorSaque -= (quantidadeNotaCinco * 5)
+            println("$quantidadeNotaCinco notas de 5")
+        }
+
+        quantidadeNotaUm = valorSaque
+        if (quantidadeNotaUm > 0) {
+            println("$quantidadeNotaUm notas de 1")
+        }
+    }
+}
